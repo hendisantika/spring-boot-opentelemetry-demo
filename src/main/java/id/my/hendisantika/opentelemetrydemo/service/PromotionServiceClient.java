0e0b246862982e177d998e4351d47f9c1f2488d1
hendisantika <hendisantika@yahoo.co.id>
2024-06-04 08:37:46 +0700

    Add getProductPromotions in PromotionServiceClient🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/opentelemetrydemo/service/PromotionServiceClient.java
+++ src/main/java/id/my/hendisantika/opentelemetrydemo/service/PromotionServiceClient.java
@@ -1,10 +1,18 @@
 package id.my.hendisantika.opentelemetrydemo.service;
 
 import id.my.hendisantika.opentelemetrydemo.config.ApplicationProperties;
+import id.my.hendisantika.opentelemetrydemo.domain.Promotion;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +31,20 @@ public class PromotionServiceClient {
 
     private final RestTemplate restTemplate;
     private final ApplicationProperties properties;
+
+    public List<Promotion> getProductPromotions() {
+        try {
+            log.info("Fetching promotions from {}", properties.getPromotionServiceUrl() + "/api/promotions");
+            HttpHeaders headers = new HttpHeaders();
+            HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+            ResponseEntity<List<Promotion>> response = restTemplate.exchange(
+                    properties.getPromotionServiceUrl() + "/api/promotions", HttpMethod.GET, httpEntity,
+                    new ParameterizedTypeReference<>() {
+                    });
+            return response.getBody();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
