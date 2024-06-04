aefc0e01696ede9ea323885aa37353e3e985e600
hendisantika <hendisantika@yahoo.co.id>
2024-06-04 08:32:43 +0700

    Add AppConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/opentelemetrydemo/config/AppConfig.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.opentelemetrydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-opentelemetry-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/4/24
+ * Time: 08:32
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class AppConfig {
+}
