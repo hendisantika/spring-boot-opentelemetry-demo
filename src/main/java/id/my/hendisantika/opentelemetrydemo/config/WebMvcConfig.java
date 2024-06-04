abcefdf818543c4456affc2fd0f05d9a0d489887
hendisantika <hendisantika@yahoo.co.id>
2024-06-04 08:34:24 +0700

    Add WebMvcConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/opentelemetrydemo/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.opentelemetrydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-opentelemetry-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/4/24
+ * Time: 08:34
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true);
+    }
+}
