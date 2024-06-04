4b01df7d9845573b247c68ca3405d7c5e7735821
hendisantika <hendisantika@yahoo.co.id>
2024-06-04 08:35:06 +0700

    Add Product Domain🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/opentelemetrydemo/domain/Product.java
@@ -0,0 +1,32 @@
+package id.my.hendisantika.opentelemetrydemo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-opentelemetry-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/4/24
+ * Time: 08:34
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
